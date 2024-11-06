13bf8f1031ef67ec54116be088bc9c2ca13b06e8
5jun99 <oj041088@gmail.com>
2024-11-06 15:39:21 +0900

    11/6

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -49,6 +49,7 @@ for entry in feed.entries:
             file.write(entry.description)  # 새 글 내용을 작성
 
 
+
         # 깃허브 커밋
         repo.git.add(file_path)
         repo.git.commit('-m', f'Add post: {entry.title}')
